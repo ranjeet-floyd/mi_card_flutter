ba354c1ec728be4cd7d670b13c5003e65737e1ad
ranjeet <ranjeet.floyd@gmail.com>
2020-04-20 10:34:32 +0530

    use StateWidget for hot reloading and container for body

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MICardApp());
+}
+
+// Stateless widget enable hot reloading
+class MICardApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Container(
+          child: Text("Hello Container"),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
